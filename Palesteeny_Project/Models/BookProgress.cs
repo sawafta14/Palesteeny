a6c0c055f0773da249c122a5f738aeae3c071d8e
safaa <ssaaffaasawafta@gmail.com>
2025-07-17 01:19:47 +0300

    Resolve conflicts and clean workspace

--- Palesteeny_Project/Models/BookProgress.cs
+++ Palesteeny_Project/Models/BookProgress.cs
@@ -5,10 +5,10 @@
         public int BookProgressId { get; set; }
 
         public int UserPalId { get; set; }
-        public UserPal UserPal { get; set; }
+        public UserPal? UserPal { get; set; }
 
         public int BookId { get; set; }
-        public Book Book { get; set; }
+        public Book? Book { get; set; }
 
         public int ProgressPercent { get; set; } // 0 to 100
     }
